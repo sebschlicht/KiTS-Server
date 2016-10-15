7bd726697ed46d8b6734c9f90be3e3e3a6953124
Sebastian Schlicht <sebastian@jablab.de>
2016-10-16 01:26:58 +0200

    application config class documented, convenience constructors added

--- src/main/java/de/jablab/sebschlicht/ApplicationConfig.java
+++ src/main/java/de/jablab/sebschlicht/ApplicationConfig.java
@@ -1,6 +1,8 @@
 package de.jablab.sebschlicht;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -9,53 +11,145 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ApplicationConfig {
+/**
+ * Base class for application configuration classes that can be loaded from
+ * property files.
+ *
+ * @author sebschlicht
+ *
+ */
+public abstract class ApplicationConfig {
 
-	/**
-	 * generated UID
-	 */
-	private static final long serialVersionUID = 8051169162112790889L;
+    /**
+     * generated UID
+     */
+    public static final long serialVersionUID = 8051169162112790889L;
 
-	private final static Logger LOG = LoggerFactory.getLogger(ApplicationConfig.class);
+    private final static Logger LOG =
+            LoggerFactory.getLogger(ApplicationConfig.class);
 
-	public void loadFromResourcesFile(String resourceName) throws IOException {
-		LOG.info("loading configuration \"" + getClass().getName() + "\" from \"" + resourceName + "\"");
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourceName);
-		if (inputStream == null) {
-			LOG.warn("Could not find resource \"" + resourceName + "\".");
-			return;
-		}
-		Properties prop = new Properties();
-		prop.load(inputStream);
+    /**
+     * Creates an (empty) application configuration.<br>
+     * If you want to immediately load its field from any supported resource,
+     * use the respective constructor instead.<br>
+     * If you create an empty application configuration, you can load its fields
+     * via one of the <code>loadFrom*</code> methods.
+     */
+    public ApplicationConfig() {
+    }
 
-		LOG.debug(getClass().getDeclaredFields().length + " configuration fields found.");
-		for (Field f : getClass().getDeclaredFields()) {
-			if (!f.isAccessible()) {
-				f.setAccessible(true);
-			}
+    /**
+     * Creates an application configuration from a file. (convenience function
+     * to )
+     *
+     * @param file
+     *            application configuration file
+     * @throws FileNotFoundException
+     *             if the file was not found
+     * @throws IOException
+     *             if the file is inaccessible
+     */
+    public ApplicationConfig(
+            File file) throws IOException {
+        loadFromFile(file);
+    }
 
-			Class<?> type = f.getType();
-			LOG.debug("loading field \"" + f.getName() + "\" of type \"" + type.getName() + "\"");
+    /**
+     * Creates an application configuration from a resource.
+     *
+     * @param name
+     *            name of the application configuration resource
+     * @throws IllegalArgumentException
+     *             if the resource was not found
+     * @throws IOException
+     *             if the resource is inaccessible
+     */
+    public ApplicationConfig(
+            String name) throws IOException {
+        loadFromResource(name);
+    }
 
-			String value = prop.getProperty(f.getName());
-			if (value == null) {
-				LOG.warn("field \"" + f.getName() + "\" is missing.");
-				continue;
-			}
+    /**
+     * Loads the application configuration from a file.
+     *
+     * @param file
+     *            application configuration file
+     * @throws IOException
+     *             if the file is inaccessible
+     */
+    public void loadFromFile(File file) throws IOException {
+        InputStream inputStream = new FileInputStream(file);
+        loadFromInputStream(inputStream);
+    }
 
-			try {
-				if (type == String.class) {
-					f.set(this, prop.getProperty(f.getName()));
-				} else if (type == File.class) {
-					f.set(this, new File(prop.getProperty(f.getName())));
-				}
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    /**
+     * Loads the application configuration from a resource.
+     *
+     * @param resourceName
+     *            name of the application configuration resource
+     * @throws IllegalArgumentException
+     *             if the resource was not found
+     * @throws IOException
+     *             if the resource is inaccessible
+     */
+    public void loadFromResource(String name) throws IOException {
+        InputStream inputStream =
+                getClass().getClassLoader().getResourceAsStream(name);
+        if (inputStream == null) {
+            throw new IllegalArgumentException(
+                    "Could not find resource \"" + name + "\"!");
+        }
+        loadFromInputStream(inputStream);
+    }
+
+    /**
+     * Loads the application configuration from an input stream.
+     *
+     * @param inputStream
+     *            application configuration input stream
+     * @throws IOException
+     *             if the stream is inaccessible
+     */
+    public void loadFromInputStream(InputStream inputStream)
+            throws IOException {
+        Properties prop = new Properties();
+        prop.load(inputStream);
+
+        LOG.trace(getClass().getDeclaredFields().length
+                + " configuration fields found.");
+        for (Field f : getClass().getDeclaredFields()) {
+            // allow fields to be private (pure configuration model)
+            if (!f.isAccessible()) {
+                f.setAccessible(true);
+            }
+
+            Class<?> type = f.getType();
+            LOG.trace("loading field \"" + f.getName() + "\" of type \""
+                    + type.getName() + "\"");
+
+            String value = prop.getProperty(f.getName());
+            if (value == null) {
+                LOG.warn("field \"" + f.getName() + "\" is missing.");
+                continue;
+            }
+
+            try {
+                if (type == String.class) {
+                    f.set(this, value);
+                } else if (type == File.class) {
+                    f.set(this, new File(value));
+                } else {
+                    // unknown field type
+                    throw new IllegalArgumentException("Type \""
+                            + type.getName() + "\" is not supported!");
+                }
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException(
+                        "Failed to set field \"" + f.getName() + "\"!", e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException(
+                        "Failed to set field \"" + f.getName() + "\"!", e);
+            }
+        }
+    }
 }
